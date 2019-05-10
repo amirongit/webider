fdac17f7f4f6a92b3c9b1ac4a6bf3c0fbc3f2176
amir <bigamir@linux.local>
2019-05-10 18:08:16 +0430

    deleted comments!

--- strlib.py
+++ strlib.py
@@ -1,13 +1,13 @@
 from random import choice
 from string import ascii_lowercase
 
-def mk_domaim(length, **kwargs):
-
-    ### - at the beginning of the job, we don't know where to start from; so I just randomly make some domains to
-    ### - go there and get other domains!
+def random_domain(length, **kwargs):
 
     """provides random domain names."""
 
     domain = str()
     for i in range(length): domain += choice(list(ascii_lowercase))
     return domain + '.' + choice(kwargs['domains'])
+
+def get_domain_str(string):
+    pass
