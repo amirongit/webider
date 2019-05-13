d743b44e7238ba8da4610e35f955249d3a68c7e8
amir <bigamir@linux.local>
2019-05-13 22:02:25 +0430

    completed the (get domain) method

--- strlib.py
+++ strlib.py
@@ -1,13 +1,22 @@
 from random import choice
 from string import ascii_lowercase
+from bs4 import BeautifulSoup
 
-def random_domain(length, **kwargs):
+def random_domain(length, *args):
 
     """provides random domain names."""
 
     domain = str()
     for i in range(length): domain += choice(list(ascii_lowercase))
-    return domain + '.' + choice(kwargs['domains'])
+    return domain + '.' + choice(list(args))
 
-def get_domain_str(string):
-    pass
+def get_domain(string):
+
+    """gets the domains of an html web page."""
+
+    tmpobj = BeautifulSoup(string, 'html.parser')
+    links = [i.attrs['href'] for i in htm.find_all('a')]
+    domains = list()
+    for i in links:
+        if 'http' in i:
+            domain.append(i.split('/')[2])
