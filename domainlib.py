c900e230ce9654bf957194deb66f7876450973cf
amir <pinoq@protonmain.com>
2019-05-23 11:10:57 +0430

    bugs fixed! (href keyword for bs4)

--- domainlib.py
+++ domainlib.py
@@ -7,7 +7,9 @@ def random_domain(*args, length=randint(3, 10)):
     """provides random domain names."""
 
     domain = str()
+    
     while len(domain) < length: domain += choice(list(ascii_lowercase))
+    
     return domain + '.' + choice(list(args))
 
 def find_domain(html_str):
@@ -15,9 +17,17 @@ def find_domain(html_str):
     """gets the domains of an html web page."""
 
     tmpobj = BeautifulSoup(html_str, 'html.parser')
-    links = [i.attrs['href'] for i in tmpobj.find_all('a')]
+    links = list()
+
+    for i in tmpobj.find_all('a'):
+            try:
+                links.append(i.attrs['href'])
+            except:
+                pass
+
     domains = list()
     for i in links:
         if 'http' in i:
             domains.append(i.split('/')[0] + '//' + i.split('/')[2])
+    
     return domains
