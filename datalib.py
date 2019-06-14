54c2fbd9d7ac0a9aff1900b517f3deb17999ac28
amir <bigamir@linux.local>
2019-06-14 16:44:51 +0430

    code fixed!

--- datalib.py
+++ datalib.py
@@ -9,11 +9,14 @@ def generate_db():
 
     return {'cursor': cursor, 'connection': connection}
 
-def generate_table(cursor):
+def generate_table(connection):
 
     "creates the needed tables in a database to use with 'webider'."
 
+    cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXIST "domains" ("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "domain"	TEXT NOT NULL);''')
+    connection.commit()
+
 
     return True
 
