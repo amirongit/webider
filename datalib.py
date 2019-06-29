fa8531356432bfa8b469a7984954ee015c0fda0d
amir <bigAmir@localhost.localdomain>
2019-06-29 13:46:55 +0430

    defined insert and read valuse from the db

--- datalib.py
+++ datalib.py
@@ -1,20 +1,35 @@
 from sqlite3 import connect
 
-def generate_db():
+"""all of the methods in this moudle are provided to be used with 'webider'!"""
+
+def generate_db(name):
     
-    "creates a database to use with 'webider'."
+    "generates a database and it's tables to use on webider!."
     
-    connection = connect('webider.db')
+    ### so we need a connection and a cursor to connect ti the databse and do some queries on it!
+    conn = connect('webider.db')
+    c = conn.cursor()
+
+    ### and the tables we need!
+    c.execute('''CREATE TABLE IF NOT EXISTS domains (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, domain TEXT NOT NULL);''')
+    conn.commit()
+
+    return {'cursor': c, 'connection': conn}
+
+def insert_domain(domain, connection):
+    
+    """inserts a domain to the table"""
+    
+    ### I don't wanna use the main cursor in the program! so just make a cursor to do with this fucntion. 
+    ### (increases memory and cpu usage a lot; but it's my choice!)
     cursor = connection.cursor()
+    cursor.execute('''INSERT INTO domains(domain) VALUES("{}")'''.format(domain))
+    cursor.close()
 
-    return {'cursor': cursor, 'connection': connection}
+def read_domains(NumberOfRows, cursor):
+    
+    """returns last X records on the domains table."""
+    
+    cursor.execute('''SELECT * FROM domains WHERE id > (SELECT max(id) FROM domains) - {}'''.format(NumberOfRows))
+    return cursor.fetchall()
 
-def generate_table(connection):
-
-    "creates the needed tables in a database to use with 'webider'."
-
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE domains (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, domain TEXT NOT NULL);''')
-    connection.commit()
-
-    return True
