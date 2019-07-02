49be94f1f3acc9d91279c5e9783eb30de360380f
amir <bigAmir@localhost.localdomain>
2019-07-02 12:47:39 +0430

    defined some methods

--- netlib.py
+++ netlib.py
@@ -2,11 +2,11 @@ from requests import session
 
 class crawler(session):
 
-    def is_ok(domain, proxies={}):
+    def is_ok(self, domain, proxy):
 
         "checks if it can access to the server or not!"
 
-        res = self.get(domain, proxies=proxies)
+        res = self.get(domain, proxies=proxy)
         if str(res) == '<Response [200]>':
 
             return True
