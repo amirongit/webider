818f0d3b71ce755225589b5200790cd2ea69f953
amir <bigAmir@localhost.localdomain>
2019-07-02 16:13:27 +0430

    im gonna clean it up!

--- datalib.py
+++ datalib.py
@@ -1,28 +1,20 @@
 from sqlite3 import connect
 
 def generate(name):
-    
     conn = connect('webider.db')
     c = conn.cursor()
-
     c.execute('''CREATE TABLE IF NOT EXISTS domains (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, domain TEXT NOT NULL UNIQUE);''')
     conn.commit()
     c.close()
-
     return conn
 
 def write(domain, cursor):
-    
     cursor.execute('''INSERT INTO `domains`('domain') VALUES("{}")'''.format(domain))
 
 def read(NumberOfRows, cursor):
-    
     cursor.execute('''SELECT * FROM `domains` WHERE id > (SELECT max(id) FROM `domains`) - {}'''.format(NumberOfRows))
-    
     return cursor.fetchall()
 
 def get_max(cursor, table):
-
     cursor.execute('''SELECT max(id) FROM `{}`'''.format(table))
-
     return cursor.fetchone()
--- domainlib.py
+++ domainlib.py
@@ -2,28 +2,21 @@ from random import choice, randint
 from string import ascii_lowercase
 from bs4 import BeautifulSoup
 
-def generate(*args, length=randint(3, 10)):
-
+def generate(*args):
     domain = str()
-    
-    while len(domain) < length: domain += choice(list(ascii_lowercase))
-    
+    while len(domain) < randint(3, 10): domain += choice(list(ascii_lowercase))
     return 'http://' + domain + '.' + choice(list(args))
 
 def surf(html_str):
-
     tmpobj = BeautifulSoup(html_str, 'html.parser')
     links = list()
-
     for i in tmpobj.find_all('a'):
             try:
                 links.append(i.attrs['href'])
             except:
                 pass
-
     domains = list()
     for i in links:
         if 'http' in i:
             domains.append(i.split('/')[0] + '//' + i.split('/')[2])
-    
     return domains
