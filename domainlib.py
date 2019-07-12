b94a1be27e8ef21fb99725565b8b2eb9d768cc90
amir <bigAmir@localhost.localdomain>
2019-07-12 17:27:13 +0430

    wrote comments!

--- domainlib.py
+++ domainlib.py
@@ -3,11 +3,18 @@ from string import ascii_lowercase
 from bs4 import BeautifulSoup
 
 def generate(*args):
+
+    "generates random domains with the domain names given!"
+
     domain = str()
     while len(domain) < randint(3, 10): domain += choice(list(ascii_lowercase))
+
     return 'http://' + domain + '.' + choice(list(args))
 
 def surf(html_str):
+
+    "returns web page urls of the given HTML string!"
+
     tmpobj = BeautifulSoup(html_str, 'html.parser')
     links = list()
     for i in tmpobj.find_all('a'):
@@ -19,4 +26,5 @@ def surf(html_str):
     for i in links:
         if 'http' in i:
             domains.append(i.split('/')[0] + '//' + i.split('/')[2])
+            
     return domains
