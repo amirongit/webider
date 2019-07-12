5f1e9693b28808bb02dd5586aa1e18324f42af2a
amir <bigAmir@localhost.localdomain>
2019-07-12 18:23:28 +0430

    defined main func!

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import datalib, domainlib
+import datalib, domainlib, surflib
 from json import load, dump
 from pyfiglet import figlet_format
+from subprocess import call
 
 print(figlet_format('w3b1der'))
 print('git: https://github.com/bigAmir/webider')
 
-def start(ConfigFile):
+def start():
 
     "configures the config file; you can do it your self!"
 
-    cfg = load(open(ConfigFile))
+    cfg = load(open('config.json'))
 
     if cfg['first_run']:
         tmp = input('(you can leave it empty)\nproxy: ')
@@ -17,11 +18,19 @@ def start(ConfigFile):
         cfg['proxies']['https'] = tmp
         cfg['first_run'] = False
         conn = datalib.generate()
-        dump(cfg, open(ConfigFile, 'w'))
+        dump(cfg, open('config.json', 'w'))
 
     else:
         conn = datalib.generate()
 
     return {'connection': conn, 'config': cfg}
 
+def main():
+    var = start()
+    call('clear', shell=True)
+    opt = input('1 - randomly create some domains!\n2 - look in the saved urls for new domains\nany other numbers - exit!')
+    
+    if opt == 1: surflib.random_surf(var['connection'], var['config']['proxies'])
+    elif opt == 2: surflib.surf_the_data(var['connection'])
+    else: exit()
 
