3aff22282683654d62790a1e47b76d7db3346d93
amir <bigAmir@localhost.localdomain>
2019-07-12 17:26:39 +0430

    wrote comments and modified generate fnc!

--- datalib.py
+++ datalib.py
@@ -1,20 +1,38 @@
 from sqlite3 import connect
 
-def generate(name):
+def generate():
+
+    """creates a database and it's tables to use; it 
+    won't delete your database if you already have one."""
+
     conn = connect('webider.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS domains (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, domain TEXT NOT NULL UNIQUE);''')
     conn.commit()
     c.close()
+
     return conn
 
 def write(domain, cursor):
+
+    """inserts to the table; don't forget to commit
+    after calling this fnc."""
+
     cursor.execute('''INSERT INTO `domains`('domain') VALUES("{}")'''.format(domain))
 
-def read(NumberOfRows, cursor):
+def read(cursor, NumberOfRows=10):
+
+    """returns a number of the selected 
+    values from data base!"""
+
     cursor.execute('''SELECT * FROM `domains` WHERE id > (SELECT max(id) FROM `domains`) - {}'''.format(NumberOfRows))
+
     return cursor.fetchall()
 
 def get_max(cursor, table):
+
+    "returns the number of the records in  databse!"
+
     cursor.execute('''SELECT max(id) FROM `{}`'''.format(table))
+    
     return cursor.fetchone()
