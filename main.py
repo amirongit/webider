be62d2f1198090c302352a3b808472ce05acbbf2
amir <bigAmir@localhost.localdomain>
2019-07-12 17:57:10 +0430

    re writing the main file!

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
 import datalib, domainlib
 from json import load, dump
 from pyfiglet import figlet_format
-from requests import get
-from random import randint
 
 print(figlet_format('w3b1der'))
 print('git: https://github.com/bigAmir/webider')
 
 def start(ConfigFile):
+
+    "configures the config file; you can do it your self!"
+
     cfg = load(open(ConfigFile))
+
     if cfg['first_run']:
         tmp = input('(you can leave it empty)\nproxy: ')
         cfg['proxies']['http'] = tmp
         cfg['proxies']['https'] = tmp
         cfg['first_run'] = False
-        conn = datalib.generate('webider.db')
+        conn = datalib.generate()
         dump(cfg, open(ConfigFile, 'w'))
+
     else:
-        conn = datalib.generate('webider.db')
-    c = conn.cursor()
-    return {'cursor': c, 'connection': conn, 'config': cfg}
+        conn = datalib.generate()
 
-def random_surf(conn, proxy):
-    cursor = conn.cursor()
-    while True:
-        tmp = domainlib.generate('com', 'ir', 'org', 'info', 'io', 'pro')
-        print(tmp)
-        try:
-            res = get(tmp, proxies=proxy)
-            if res.ok == True:
-                datalib.write(tmp, cursor)
-                conn.commit()
-                print(tmp)
-        except:
-            pass
+    return {'connection': conn, 'config': cfg}
 
 
-
-
-crawler = start('config.json')
-random_surf(crawler['connection'], crawler['proxies'])
