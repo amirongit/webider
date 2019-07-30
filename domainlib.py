8e3f9e9de15da7c36170c028eb70bca29b8e2c95
bigAmir <bigAmir@localhost.localdomain>
2019-07-30 14:40:49 +0430

    wrote a class, included pep8 style and better naming.

--- domainlib.py
+++ domainlib.py
@@ -2,29 +2,53 @@ from random import choice, randint
 from string import ascii_lowercase
 from bs4 import BeautifulSoup
 
-def generate(*args):
 
-    "generates random domains with the domain names given!"
+class WebPage(object):
+    
+    def __init__(self, html_str):
+        
+        self.HTML_STRING = html_str
+
+    def get_domains(self):
+
+        "returns urls of the given HTML string!"
+
+        tmpobj = BeautifulSoup(self.HTML_STRING, 'html.parser')
+        
+        urls = list()
+
+        for url in tmpobj.find_all('a'):
+            
+            try:
+                
+                urls.append(url.attrs['href'])
+            except:
+                
+                pass
+        
+        domain_names = list()
+
+        for url in urls:
+            
+            if 'http' in url: 
+
+                domain_names.append(url.split('/')[0] + '//' + url.split('/')[2])
+        
+        return domain_names
+
+    def __repr__(self):
+
+        return str(self.HTML_STRING)
+
+
+def generate(*args, max_length=8):
+
+    "generates random domain names."
 
     domain = str()
-    while len(domain) < randint(3, 10): domain += choice(list(ascii_lowercase))
+    while len(domain) < randint(3, max_length):
+
+        domain += choice(list(ascii_lowercase))
 
     return 'http://' + domain + '.' + choice(list(args))
-
-def surf(html_str):
-
-    "returns web page urls of the given HTML string!"
-
-    tmpobj = BeautifulSoup(html_str, 'html.parser')
-    links = list()
-    for i in tmpobj.find_all('a'):
-        try:
-            links.append(i.attrs['href'])
-        except:
-            pass
-    domains = list()
-    for i in links:
-        if 'http' in i:
-            domains.append(i.split('/') + '//' + i.split('/')[2])
-
-    return domains
+    
