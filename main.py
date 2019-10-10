0c03ea4cec909386cd0185ba82168e7019ced257
amir <pinoq@protonmail.com>
2019-10-10 23:53:49 +0330

    back to work! :D

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import libs
+from libs import stringLib, ormLib
+from json import dump, load
+from requests import get
+
+cfg = load(open('cfg/settings.json'))
+
+def setup():
+    pass
+
+def main():
+    pass
+
+if cfg['first_run'] == True:
+    setup()
+
+if __name__ == 'main':
+    main()
