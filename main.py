01f11ac65a7817b9c2db5f8431ef33a2713b97ac
amir <pinoq@protonmail.com>
2019-10-20 11:26:27 +0330

    still in the main fucntion progress! now I can fill the domain pool.

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ call('clear', shell=True)
 with open('cfg/settings.json') as config_file: cfg = load(config_file)
 
 def setup(config):
-    
+
     print('running setup...')
     config['first_run'] = False
-    
+
     proxy = input('(leave empty if you don\'t have one!)\nexample: sokcs5://127.0.0.1:9050\nproxy: ')
 
     config['proxies']['https'] = proxy
     config['proxies']['http'] = proxy
-    
+
     global cfg
     with open('cfg/settings.json', 'x') as config_file:
-        
+
         dump(config, config_file)
         cfg = load(config_file)
 
@@ -33,39 +33,65 @@ def main(first_run=False):
 
     domain_pool = list()
     if first_run == True:
-    
+
         print('at the first we need a webpage to create our domain pool!\nif you have one, enter it; if you don\'t have a url to start with, you can type \'random\' to generate a random domain!')
         starting_domain = input('\nstarting url: ')
 
-        while True:
+        if starting_domain == 'random':
 
-        if starting_domain == 'random': 
-        
-            starting_domain = stringLib.generate('ir', 'com', 'org', 'net', 'us', 'uk', 'tk')
-            res = get(starting_domain)
-            if res.status_code == 200: domain_pool.append(starting_domain)
+            while True:
+
+                starting_domain = stringLib.generate('ir', 'com', 'org', 'net', 'us', 'uk', 'tk')
+                res = get(starting_domain)
+                if res.status_code == 200:
+
+                    domain_pool.append(starting_domain)
+                    data_base_connection = ormLib.create_data_base()
+                    break
+
 
         else:
 
             res = get(starting_domain)
-            if res.status_code == 200: domain_pool.append(starting_domain)
+            if res.status_code == 200:
+
+                domain_pool.append(starting_domain)
+                data_base_connection = ormLib.create_data_base()
 
     else:
 
         data_base_connection = ormLib.create_data_base()
-        data_base_cursor = data_base_connection.cursor()
 
-        for domain in ormLib.get_all_domains(data_base_cursor): domain_pool.append(domain)
+    data_base_cursor = data_base_connection.cursor()
+    try:
 
+        ormLib.insert_new_domain(data_base_cursor, domain_pool[0])
+    except:
+
+        pass
+
+    domain_pool = dict()
+    for ID, domain in ormLib.get_all_domains(): domain_pool[ID] = domain
+
+    for url in domain_pool:
+
+        res = get(url[1])
+        # get the id of domain in data base to mark as surfed
+        tmp_page = stringLib.WebPage(res.text)
+        tmp_domain_list = tmp_page.get_domains()
+
+        for domain in tmp_domain_list: ormLib.insert_new_domain(data_base_cursor, domain)
 
 
 if __name__ == 'main':
-    
+
     if cfg['first_run'] == True:
-        
+
         setup()
         main(True)
-    
+
     else:
 
         main()
+
+    # write interupt statement
