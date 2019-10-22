209adc6e6cc84d11d1f00e4a57adadd7652a04ac
amir <pinoq@protonmail.com>
2019-10-22 20:41:39 +0330

    removed uniqeness from fields!

--- libs/ormLib.py
+++ libs/ormLib.py
@@ -5,7 +5,7 @@ def create_data_base(path):
     connection = sqlite3.connect(f'{path}/webider.db')
     cursor = connection.cursor()
 
-    cursor.execute('CREATE TABLE IF NOT EXISTS domains("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "domain" TEXT NOT NULL UNIQUE);')
+    cursor.execute('CREATE TABLE IF NOT EXISTS domains("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "domain" TEXT NOT NULL);')
     cursor.close()
 
     return connection
