6a149d28c5f08c71cd0a8da8053c1aeb1ede5939
amir <pinoq@protonmail.com>
2019-10-24 12:41:47 +0330

    added id_greater_than to the select function!

--- libs/ormLib.py
+++ libs/ormLib.py
@@ -15,9 +15,9 @@ def insert_new_domain(cursor, domain):
 
     cursor.execute('INSERT INTO domains(domain) VALUES(?)', (domain,))
 
-#add the id grater from argument to escape the surfed pages!
-def get_all_domains(cursor):
 
-    cursor.execute('SELECT * FROM domains')
+def get_all_domains(cursor, id_greater_than=0):
+
+    cursor.execute('SELECT * FROM domains WHERE id > ?', (id_greater_than,))
 
     return cursor.fetchall()
