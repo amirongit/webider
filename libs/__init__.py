075e6341fe79ce9cb661a5f8694c2093fd16771c
amir <pinoq@protonmail.com>
2019-10-25 17:33:04 +0330

    modified init docstring.

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,4 +1,5 @@
-"""this file imports the other modules to make this dir an importable pkg."""
+"""this pkg includes twi modules named orm_lib and string_lib;
+to get more type 'help(libs.<modulename>.)'"""
 
 from . import orm_lib
 from . import string_lib
