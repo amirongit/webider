d16a3aa26ee0bf4ff7e844cd625484846d455baa
amir <pinoq@protonmail.com>
2019-10-25 17:34:26 +0330

    fixed typing mistake.

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,4 +1,4 @@
-"""this pkg includes twi modules named orm_lib and string_lib;
+"""this pkg includes two modules named orm_lib and string_lib;
 to get more type 'help(libs.<modulename>.)'"""
 
 from . import orm_lib
