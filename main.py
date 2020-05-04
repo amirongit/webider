c28966ed6c0bfd2533d31b6bdce42e6cadc6cffe
mr.pino <pinoq@protonmail.com>
2020-05-04 13:45:24 +0430

    defined generat random url function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from os.path import dirname, abspath
 from json import dump, load
+from os.path import dirname, abspath
 from requests import get
+from random import randint, choice
+from string import ascii_lowercase
+
 from sqlalchemy import create_engine, Integer, Column, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from bs4 import BeautifulSoup
@@ -29,3 +32,11 @@ def get_urls(plain_html):
     for a_tag in vanilla_bs4.find_all('a'):
         urls.append(a_tag.attrs['href'])
     return list(filter(lambda s: True if 'http' in s else False, urls))
+
+
+def generate_url(min_length=3, max_length=10, domain_names='com,net,org'):
+    url = str()
+    for i in rang(randint(min_length, max_length)):
+        url += choice(list(ascii_lowercase))
+    url += '.' + choice(list(domain_names.split(',')))
+    return url
