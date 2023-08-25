8cd6e1b5c6fbcd810a3949d3a407832928a08548
amirhossein ahmadi <amirthehossein@gmail.com>
2023-08-25 17:31:00 +0330

    add get method to domain repository

--- src/domain_repository.py
+++ src/domain_repository.py
@@ -1,12 +1,13 @@
 import sqlite3
 
-from typing import NoReturn
+from typing import Any, NoReturn
 
-from utils import Singleton, py_to_sql_bool
+import utils
+
+from dto import DomainDTO, DomainQueryDTO
 
 
-class DomainRepository(metaclass=Singleton):
-    # TODO: add ability to mark the domain as unsurfable
+class DomainRepository(metaclass=utils.Singleton):
     def __init__(self, db_uri: str) -> NoReturn:
         self.db_uri: str = db_uri
         self._create_database_schema()
@@ -19,27 +20,44 @@ class DomainRepository(metaclass=Singleton):
                 'surfed BOOLEAN NOT NULL DEFAULT FALSE, url STRING NOT NULL UNIQUE);'
             )
 
-    def get_all_not_surfed_domains_ordered_by_id(
-        self,
-        desc: bool = True
-    ) -> list[tuple[int, str]]:
+    def get(self, domain_query: DomainQueryDTO) -> list[DomainDTO]:
         with sqlite3.connect(self.db_uri) as conn:
+            conn.row_factory = utils.domain_row_factory
             cursor: sqlite3.Cursor = conn.cursor()
-            return cursor.execute(
-                'SELECT id, url FROM domains WHERE surfed = FALSE '
-                f'ORDER BY id {"DESC" if desc else "ASC"};'
-            ).fetchall()
+            return cursor.execute(self._compile_query(domain_query)).fetchall()
 
-    def update_domain_surfed_status_by_id(
-        self,
-        id_: int,
-        surfed: bool
-    ) -> NoReturn:
-        with sqlite3.connect(self.db_uri) as conn:
-            cursor: sqlite3.Cursor = conn.cursor()
-            cursor.execute(f'UPDATE domains SET surfed = {py_to_sql_bool(surfed)} WHERE id = {id_};')
+    def create_or_update(self, domain: DomainDTO) -> NoReturn:
+        self.create(domain) if domain.id is None else self.update(domain)
 
-    def create_domain(self, url: str, surfed: bool = False) -> NoReturn:
+    def update(self, domain: DomainDTO) -> NoReturn:
         with sqlite3.connect(self.db_uri) as conn:
             cursor: sqlite3.Cursor = conn.cursor()
-            cursor.execute(f'INSERT INTO domains(surfed, url) VALUES({py_to_sql_bool(surfed)}, \'{url}\');')
+            cursor.execute(
+                f'UPDATE domains SET surfed = {str(domain.surfed)}, '
+                f'url = \'{domain.url}\' WHERE id = {domain.surfed};'
+            )
+
+    def create(self, domain: DomainDTO) -> NoReturn:
+        with sqlite3.connect(self.db_uri) as conn:
+            cursor: sqlite3.Cursor = conn.cursor()
+            cursor.execute(
+                f'INSERT INTO domains(surfed, url) VALUES({str(domain.surfed)}, \'{domain.url}\');'
+            )
+
+    def _compile_query(self, domain_query: DomainQueryDTO):
+        query = 'SELECT * FROM domains '
+
+        if len(constraints := list(filter(lambda x: x[1] is not None, domain_query.__dict__.items()))) > 0:
+            query_conditions = list()
+
+            for condition, value in constraints:
+                if condition == 'id':
+                    query_conditions.append(f'id = {str(value)}')
+                elif condition == 'url':
+                    query_conditions.append(f'url LIKE \'%{value}%\'')
+                elif condition == 'surfed':
+                    query_conditions.append(f'surfed = {str(value)}')
+
+            query += 'WHERE ' + ' AND '.join(query_conditions)
+
+        return query + ';'
